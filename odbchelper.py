489a490cc086fd92bc1b4e2c3b721e472d77c3f3
Muhammad Hafidz Misrudin <halfpheez92@gmail.com>
2017-10-25 13:27:02 +1000

    added a test module function with main()

--- odbchelper.py
+++ odbchelper.py
@@ -3,3 +3,11 @@ def buildConnectionString(params):
 
     Returns string."""
     return ";".join(["%s = %s " % (k, v) for k, v in params.items()])
+
+if __name__ == "__main__":
+    myParams = {"server":"mpilgrim", \
+    "database":"master", \
+    "uid":"sa", \
+    "pwd":"secret" \
+    }
+    print buildConnectionString(myParams)
