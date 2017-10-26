2cc571c0314fac29d6dc171060808f7528e657cc
Muhammad Hafidz Misrudin <halfpheez92@gmail.com>
2017-10-26 22:29:38 +1000

    adjust return statement with connection string.

--- odbchelper.py
+++ odbchelper.py
@@ -2,12 +2,12 @@ def buildConnectionString(params):
     """Build a connection string from a dictionary of parameters.
 
     Returns string."""
-    return "; ".join(["%s = %s" % (k, v) for k, v in params.items()])
+    return "The connection strings are as follows:\n" + "; ".join(["%s = %s" % (k, v) for k, v in params.items()]);
 
 if __name__ == "__main__":
     myParams = {"server":"mpilgrim", \
     "database":"master", \
     "uid":"sa", \
-    "pwd":"secret" \
+    "pwd":"/home/lordharambe/" \
     }
-    print buildConnectionString(myParams)
+    print buildConnectionString(myParams);
