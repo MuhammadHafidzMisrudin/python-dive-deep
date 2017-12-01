58740057b59e3c2deb2706b8e0001710170c28e5
Muhammad Hafidz Misrudin <halfpheez92@gmail.com>
2017-12-02 00:10:51 +1000

    added a framework (implementation) for App module in lpoApp.py.

--- src/problem1/lpoApp.py
+++ src/problem1/lpoApp.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
 # lpoApp.py by Muhammad Hafidz
-# App module python file the Lake Pend Oreille 
+# App module python file the Lake Pend Oreille
+
+from tkinter import *
+from tkinter import ttk, messagebox
+from statistics import mean, median
+from datetime import date
+
+
+class lpoApp:
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
