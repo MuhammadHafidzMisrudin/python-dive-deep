30eebdd3b321b8e5f390a2ba1f2b679824957dd9
Muhammad Hafidz Misrudin <halfpheez92@gmail.com>
2018-01-06 01:18:49 +1000

    implemented lpoDB.py. added framework components (functions) in lpoDB class in lpoDB.py.

--- src/problem1/lpoDB.py
+++ src/problem1/lpoDB.py
@@ -5,3 +5,25 @@ from datetime import date, datetime, timedelta
 from tkinter import messagebox
 import sqlite3
 import lpoWeb
+
+class lpoDB():
+    def __init__(self, **kwargs):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def get_data_for_range(self, start, end):
+        pass
+
+    def _get_status_for_range(self, start. end):
+        pass
+
+    def _update_data_for_date(self, date, partial):
+        pass
+
+    def clear(self):
+        pass
+
+    def close(self):
+        pass
