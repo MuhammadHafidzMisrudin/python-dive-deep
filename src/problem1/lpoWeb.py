0c3bca2c97a3f4dcf12e497c393f1983d1be7cda
Muhammad Hafidz Misrudin <halfpheez92@gmail.com>
2018-02-13 20:16:36 +1000

    added/created few more lpoWeb modules functions for data retrieval pre 2007 and post 2006 and test method in lpoWeb module.

--- src/problem1/lpoWeb.py
+++ src/problem1/lpoWeb.py
@@ -2,3 +2,21 @@ from datetime import date
 from urllib import request
 
 BASE_URL = 'http://lpo.dt.navy.mil/data/DM'
+
+def get_data_for_date(date):
+    if date.year < 2007:
+        return None
+    else:
+        return None
+
+def _get_data_pre2007(date):
+    pass
+
+def _get_data_post2006(date):
+    pass
+
+def _test():
+    pass
+
+if __name__ == '__main__':
+    _test()
