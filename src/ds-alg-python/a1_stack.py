9746e8d4b1d3f863009fa0228b4606cfa74cee7d
Hafidz <halfpheez92@gmail.com>
2018-10-26 07:45:18 +0800

    modified implementation of Stack.

--- src/ds-alg-python/a1_stack.py
+++ src/ds-alg-python/a1_stack.py
@@ -6,13 +6,27 @@ class Stack(object):
 
     def add(self, dataval):
         if dataval not in self.stack:
-            self.stack.append(dataval)
+            self.stack.append(dataval) # Add element to top of stack.
             return True
         else:
             return False
 
-    def peek(self, index):
-        return self.stack[index]
+    def remove(self):
+        if len(self.stack) <= 0:
+            return ('No Element in the Stack')
+        self.stack.pop() # Remove element from top of stack.
+
+    def peek(self):
+        # Get the value of the top element without removing it.
+        if self.stack == []:
+            raise Exception('Stack Empty!')
+        return self.stack[len(self.stack)-1]
+
+    def size(self):
+        return len(self.stack)
+
+    def display_stack(self):
+        return self.stack
 
 
 def main():
@@ -21,8 +35,20 @@ def main():
     myStack = Stack()
     myStack.add('Muhammad')
     myStack.add('Inma')
-    print(myStack.stack)
-    print(myStack.peek(1))
+    myStack.add('Reyes')
+    myStack.add('Guerrera')
+    print('all elements in a stack = ', myStack.display_stack())
+    print('peek stack now, ', myStack.peek())
+    print('remove an element on the top of stack now, ', myStack.remove())
+    print('all elements in a stack = ', myStack.display_stack())
+    print('peek stack now, ', myStack.peek())
+    print('remove an element 1th on the top of stack now, ', myStack.remove())
+    print('remove an element 0th on the top of stack now, ', myStack.remove())
+    print('all elements in a stack = ', myStack.display_stack())
+    print('peek stack now, ', myStack.peek())
+    myStack.add('Inma')
+    myStack.add('Hafidz')
+    print('all elements in a stack = ', myStack.display_stack())
 
 
 if __name__ == '__main__':
